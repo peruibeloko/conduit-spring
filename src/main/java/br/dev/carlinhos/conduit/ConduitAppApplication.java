12ef3fde81b4d84cbdb65d00a78f218276c24ceb
peruibeloko <cmendonca32@gmail.com>
2025-07-01 17:03:09 -0300

    init

---
+++ src/main/java/br/dev/carlinhos/conduit/ConduitAppApplication.java
@@ -0,0 +1,13 @@
+package br.dev.carlinhos.conduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/carlinhos/conduit/ConduitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.carlinhos.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
